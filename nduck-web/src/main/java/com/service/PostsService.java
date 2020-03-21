e921670beaf426aa8cad5e51d81cf6ccca727235
gonghojin <yolip51@naver.com>
2020-03-21 19:05:39 +0900

    Add: 테스트 기본 진행중 1

--- nduck-web/src/main/java/com/service/PostsService.java
+++ nduck-web/src/main/java/com/service/PostsService.java
@@ -13,6 +13,8 @@ import lombok.AllArgsConstructor;
 
 @Service
 @Transactional
+// 필드 DI보다는 생성자 DI를 사용하자.
+// Why?) https://developerhjg.tistory.com/205
 @AllArgsConstructor
 public class PostsService {
 
@@ -20,5 +22,19 @@ public class PostsService {
 
 	public Stream<Posts> findAll() {
 		return postsRepository.findAll().stream();
+	}
+
+	public Posts saveOnePost(Posts posts) {
+		if (posts == null) {
+			return posts;
+		}
+		Posts post = postsRepository.save(posts);
+
+		return post;
+	}
+
+	public Posts findOnePostById(Long id) {
+		Posts post = postsRepository.findPostById(id);
+		return post;
 	}
 }
--- nduck-web/src/test/java/com/web/service/PostServiceTest.java
+++ nduck-web/src/test/java/com/web/service/PostServiceTest.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 
 import com.domain.posts.PostsRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +16,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.domain.posts.Posts;
 import com.service.PostsService;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostServiceTest {
 	@Autowired
 	private PostsService postsService;
 
-	@Autowired
-	private PostsRepository postsRepository;
+	// 테스트 진행 중에 선행되어야 할 작업
+	// 단어 뜻 그대로 실질직인 테스트 로직 돌기 전에 먼저 실행됨
+	@Before
+	public void 포스트_저장() {
+		Posts post = 포스트_엔티티_생성();
+		postsService.saveOnePost(post);
+	}
+
+	private Posts 포스트_엔티티_생성() {
+		Posts post = Posts.builder()
+				.id(1L)
+				.title("토이프로젝트")
+				.author("Gongdel")
+				.content("n-duck")
+				.build();
+		return post;
+	}
 
 	@Test
 	public void 포스트_전체검색_테스트() {
@@ -37,5 +55,20 @@ public class PostServiceTest {
 		///  테스트 데이터 5개
 		Assert.assertTrue(size == 5);
 		Assert.assertFalse(size == 4);
+	}
+
+	@Test
+	public void 포스트_ID조회_테스트() {
+		// Given
+		Posts post = 포스트_엔티티_생성();
+
+		// When
+		Posts postById = postsService.findOnePostById(post.getId());
+
+		// Then
+		assertThat(post.getAuthor()).isEqualTo(postById.getAuthor());
+		assertThat(post.getContent()).isEqualTo(postById.getContent());
+		assertThat(post.getTitle()).isEqualTo(postById.getTitle());
+
 	}
 }
